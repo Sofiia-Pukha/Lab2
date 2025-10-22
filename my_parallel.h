73d16834a5a6af2b15dbe0b2c65c203c993763f5
Sofiia-Pukha <sofiia.pukha@gmail.com>
2025-10-23 01:10:50 +0300

    declare my_parallel_transform function

--- my_parallel.h
+++ my_parallel.h
@@ -1,1 +1,11 @@
+#pragma once 
 
+#include <vector>
+#include <functional>
+
+void my_parallel_transform(
+    const std::vector<double>& input,
+    std::vector<double>& output,
+    const std::function<double(double)>& op,
+    unsigned int num_threads
+);
