75de5ede2a315c77dc45f3d204b3230463316931
Sofiia-Pukha <sofiia.pukha@gmail.com>
2025-10-26 21:43:47 +0200

    Add time measurement and tests std::transform
    - Add time_operation template function 
    - Implemented tests for std::transform without policy
    - Implemented tests for std::transform with policies

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <algorithm> 
 #include <chrono> 
 #include <random>
@@ -8,8 +9,9 @@
 #include <numeric> 
 #include <cmath>
 #include <iomanip>
+#include <functional>
+#include <limits>
 #include "my_parallel.h"
-
 
 std::vector<double> generate_data(size_t size)
 {
@@ -40,11 +42,23 @@ for (int i = 0; i < 50; ++i)
 return result;
 }
 
+template <typename Func>
+double time_operation(Func f)
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    f();
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> duration = end - start;
+    return duration.count();
+}
+
 int main() {
     std::cout << "Laboratory Work: Research transform " << std::endl;
     std::cout << "------------------------------------" << std::endl;
  
     std::vector<size_t> data_sizes = {100000, 1000000, 5000000};
+    unsigned int num_cores = std::thread::hardware_concurrency();
+    std::cout << "Number of available cores: " << num_cores << std::endl;
 
     for(size_t size : data_sizes)
     {
@@ -55,7 +69,30 @@ int main() {
         auto data = generate_data(size);
     std::vector <double> result(size);
 
-    std::cout << "Running tests for this data size..." << std::endl;
+    std::cout << std::fixed << std::setprecision(4);
+
+    double time_ms;
+
+std::cout << "--- Fast Operation ---" << std::endl;
+time_ms = time_operation([&]() {
+    std::transform(data.begin(), data.end(), result.begin(), fast_op);
+});
+std::cout << "1) std::transform (no policy): " << std::setw(10) << time_ms << " ms" << std::endl;
+
+time_ms = time_operation([&]() {
+    std::transform(std::execution::seq, data.begin(), data.end(), result.begin(), fast_op);
+});
+std::cout << " 2a) std:: transform (std::execution::seq): " << std::setw(10) << time_ms << " ms" << std::endl;
+
+    time_ms = time_operation([&]() {
+            std::transform(std::execution::par, data.begin(), data.end(), result.begin(), fast_op);
+        });
+        std::cout << "2b) std::transform (std::execution::par):" << std::setw(10) << time_ms << " ms" << std::endl;
+
+        time_ms = time_operation([&]() {
+            std::transform(std::execution::par_unseq, data.begin(), data.end(), result.begin(), fast_op);
+        });
+        std::cout << "2c) std::transform (std::execution::par_unseq):" << std::setw(10) << time_ms << " ms" << std::endl;
     }
     return 0;
 }
